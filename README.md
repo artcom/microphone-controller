b0e00b02ebb65c6a8441215572d8f0aed36a0fe8
Stefan Dieterle <stefand@artcom.de>
2024-05-07 14:18:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Microphone Controller
+
+Controls the microphone volume of a Windows machine in response to MQTT messages from a broker provided by a [bootstrap-server](https://github.com/artcom/bootstrap-server).
+
+## Usage
+### Configuration
+#### Configuration file
+
+Copy `config.json.template` to `config.json` and edit to fit your environment.
+
+#### Command line arguments
+
+- configFile=/path/to/config/file (default=config.json)
+
+### Start service
+
+```bash
+npm install
+npm start
+```
+
+### Build executable
+
+```bash
+npm install -g @yao-pkg/pkg
+pkg package.json
+```
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const audio = require("win-audio")
+const { setTimeout } = require("timers/promises")
+const bootstrap = require("@artcom/bootstrap-client")
+const fs = require("fs")
+const config = JSON.parse(fs.readFileSync(readConfigFileArg()))
+
+async function main() {
+  const { logger, mqttClient } = await bootstrap(config.bootstrapServerUri, "microphoneController")
+  logger.info("Config", config)
+  mqttClient.on("offline", () => logger.error("Client is offline, Trying to reconnect"))
+  const microphone = audio.mic
+  await mqttClient.publish(config.currentVolumeTopic, microphone.get())
+  mqttClient.subscribe(config.setVolumeTopic, async (message) => {
+    logger.info("Setting microphone volume", { volume: message })
+    if (microphone.get() !== -1) {
+      microphone.set(message)
+      while (microphone.get() !== message) {
+        await setTimeout(100)
+        microphone.set(message)
+      }
+      await mqttClient.publish(config.currentVolumeTopic, microphone.get())
+    }
+  })
+}
+
+function readConfigFileArg() {
+  const args = process.argv.slice(2)[0]?.split("=")
+
+  if (args && args.length === 2 && args[0] === "configFile") {
+    return args[1]
+  } else {
+    return "config.json"
+  }
+}
+
+main()
