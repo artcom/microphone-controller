1356214b81eb3d994ddd0d505b601bba0636efc9
Stefan Dieterle <stefand@artcom.de>
2024-06-20 14:30:24 +0200

    Remove volume control and packaging

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Microphone Controller
 
-Controls the microphone gain of a Windows machine in response to MQTT messages from a broker provided by a [bootstrap-server](https://github.com/artcom/bootstrap-server).
+Enables or disables the microphone of a Windows machine in response to MQTT messages from a broker provided by a [bootstrap-server](https://github.com/artcom/bootstrap-server).
 
 ## Usage
+
 ### Configuration
+
 #### Configuration file
 
 Copy `config.json.template` to `config.json` and edit to fit your environment.
@@ -17,11 +19,4 @@ Copy `config.json.template` to `config.json` and edit to fit your environment.
 ```bash
 npm install
 npm start
-```
-
-### Build executable
-
-```bash
-npm install -g @yao-pkg/pkg
-pkg package.json
 ```
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-const audio = require("win-audio")
 const wincmd = require("node-windows")
-const { setTimeout } = require("timers/promises")
 const bootstrap = require("@artcom/bootstrap-client")
 const fs = require("fs")
 const config = JSON.parse(fs.readFileSync(readConfigFileArg()))
@@ -13,29 +11,16 @@ async function main() {
   wincmd.elevate(
     `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName} *'} | Enable-PnpDevice -Confirm:$false"`,
   )
-  const microphone = audio.mic
-  await mqttClient.publish(config.currentGainTopic, microphone.get())
-  mqttClient.subscribe(config.setGainTopic, async (message) => {
-    logger.info("Setting microphone gain", { gain: message })
-    if (microphone.get() !== -1) {
-      microphone.set(message)
-      while (microphone.get() !== message) {
-        await setTimeout(100)
-        microphone.set(message)
-      }
-      await mqttClient.publish(config.currentGainTopic, microphone.get())
-    }
-  })
 
   mqttClient.subscribe(config.disableTopic, async () => {
-    logger.info("Disabling device")
+    logger.info("Disabling device", { device: config.microphoneName })
     wincmd.elevate(
       `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName} *'} | Disable-PnpDevice -Confirm:$false"`,
     )
   })
 
   mqttClient.subscribe(config.enableTopic, () => {
-    logger.info("Enabling device")
+    logger.info("Enabling device", { device: config.microphoneName })
     wincmd.elevate(
       `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName} *'} | Enable-PnpDevice -Confirm:$false"`,
     )
