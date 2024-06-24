0decc72544363ba6bf12c8dc680ef3e6de290b49
Stefan Dieterle <stefand@artcom.de>
2024-06-24 10:47:41 +0200

    Remove whitespace

--- src/index.js
+++ src/index.js
@@ -9,20 +9,20 @@ async function main() {
   mqttClient.on("offline", () => logger.error("Client is offline, Trying to reconnect"))
   logger.info("Enabling device", { device: config.microphoneName })
   wincmd.elevate(
-    `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName} *'} | Enable-PnpDevice -Confirm:$false"`,
+    `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName}*'} | Enable-PnpDevice -Confirm:$false"`,
   )
 
   mqttClient.subscribe(config.disableTopic, async () => {
     logger.info("Disabling device", { device: config.microphoneName })
     wincmd.elevate(
-      `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName} *'} | Disable-PnpDevice -Confirm:$false"`,
+      `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName}*'} | Disable-PnpDevice -Confirm:$false"`,
     )
   })
 
   mqttClient.subscribe(config.enableTopic, () => {
     logger.info("Enabling device", { device: config.microphoneName })
     wincmd.elevate(
-      `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName} *'} | Enable-PnpDevice -Confirm:$false"`,
+      `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName}*'} | Enable-PnpDevice -Confirm:$false"`,
     )
   })
 }
