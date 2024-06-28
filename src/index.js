c62788d15b9dba1dee0fe43e2a0fb413189fdf80
Stefan Dieterle <stefand@artcom.de>
2024-06-28 11:00:51 +0200

    Check microphone name

--- src/index.js
+++ src/index.js
@@ -6,25 +6,29 @@ const config = JSON.parse(fs.readFileSync(readConfigFileArg()))
 async function main() {
   const { logger, mqttClient } = await bootstrap(config.bootstrapServerUri, "microphoneController")
   logger.info("Config", config)
-  mqttClient.on("offline", () => logger.error("Client is offline, Trying to reconnect"))
-  logger.info("Enabling device", { device: config.microphoneName })
-  wincmd.elevate(
-    `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -Is '${config.microphoneName}'} | Enable-PnpDevice -Confirm:$false"`,
-  )
-
-  mqttClient.subscribe(config.disableTopic, async () => {
-    logger.info("Disabling device", { device: config.microphoneName })
-    wincmd.elevate(
-      `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -Is '${config.microphoneName}'} | Disable-PnpDevice -Confirm:$false"`,
-    )
-  })
-
-  mqttClient.subscribe(config.enableTopic, () => {
+  if (!config.microphoneName || config.microphoneName.length === 0) {
+    logger.error("No microphone name provided")
+  } else {
+    mqttClient.on("offline", () => logger.error("Client is offline, Trying to reconnect"))
     logger.info("Enabling device", { device: config.microphoneName })
     wincmd.elevate(
-      `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -Is '${config.microphoneName}'} | Enable-PnpDevice -Confirm:$false"`,
+      `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName}*'} | Enable-PnpDevice -Confirm:$false"`,
     )
-  })
+
+    mqttClient.subscribe(config.disableTopic, async () => {
+      logger.info("Disabling device", { device: config.microphoneName })
+      wincmd.elevate(
+        `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName}*'} | Disable-PnpDevice -Confirm:$false"`,
+      )
+    })
+
+    mqttClient.subscribe(config.enableTopic, () => {
+      logger.info("Enabling device", { device: config.microphoneName })
+      wincmd.elevate(
+        `powershell.exe "Get-PnpDevice | Where-Object {$_.FriendlyName -like '${config.microphoneName}*'} | Enable-PnpDevice -Confirm:$false"`,
+      )
+    })
+  }
 }
 
 function readConfigFileArg() {
