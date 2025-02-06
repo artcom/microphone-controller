54e77f619d02e58d32a6a3da10882631c8241edd
Philipp Niederlag <philipp.niederlag@artcom.de>
2025-02-06 16:32:25 +0100

    Remove config check

--- src/index.js
+++ src/index.js
@@ -7,35 +7,31 @@ async function main() {
   const { mqttClient } = await bootstrap(config.bootstrapServerUri, "microphoneController")
   logger.info("Config", config)
 
-  if (!config.microphoneName || config.microphoneName.length === 0) {
-    logger.error("No microphone name provided")
-  } else {
-    mqttClient.on("offline", () => logger.error("Client is offline, Trying to reconnect"))
+  mqttClient.on("offline", () => logger.error("Client is offline, Trying to reconnect"))
+  logger.info("UnMuting device", { device: config.microphoneName })
+  wincmd.elevate(`"${config.svclExePath}" /UnMute "${config.microphoneName}"`)
+
+  mqttClient.subscribe(config.muteTopic, async () => {
+    logger.info("Muting device", { device: config.microphoneName })
+
+    performance.mark("commandStart")
+    wincmd.elevate(
+      `"${config.svclExePath}" /Mute "${config.microphoneName}"`,
+      { timeout: config.commandTimeout },
+      commandCallback,
+    )
+  })
+
+  mqttClient.subscribe(config.unMuteTopic, () => {
     logger.info("UnMuting device", { device: config.microphoneName })
-    wincmd.elevate(`"${config.svclExePath}" /UnMute "${config.microphoneName}"`)
 
-    mqttClient.subscribe(config.muteTopic, async () => {
-      logger.info("Muting device", { device: config.microphoneName })
-
-      performance.mark("commandStart")
-      wincmd.elevate(
-        `"${config.svclExePath}" /Mute "${config.microphoneName}"`,
-        { timeout: config.commandTimeout },
-        commandCallback,
-      )
-    })
-
-    mqttClient.subscribe(config.unMuteTopic, () => {
-      logger.info("UnMuting device", { device: config.microphoneName })
-
-      performance.mark("commandStart")
-      wincmd.elevate(
-        `"${config.svclExePath}" /UnMute "${config.microphoneName}"`,
-        { timeout: config.commandTimeout },
-        commandCallback,
-      )
-    })
-  }
+    performance.mark("commandStart")
+    wincmd.elevate(
+      `"${config.svclExePath}" /UnMute "${config.microphoneName}"`,
+      { timeout: config.commandTimeout },
+      commandCallback,
+    )
+  })
 }
 
 function commandCallback(error) {
